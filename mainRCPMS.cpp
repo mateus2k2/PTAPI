db99d28b1eb1b9dcbbba9e94dd216be0ef328e40
JavaCaeLa <andlui.eng@gmail.com>
2024-02-25 19:15:10 -0300

    Upload Files

---
+++ mainRCPMS.cpp
@@ -0,0 +1,84 @@
+/*
+ * main.cpp
+ * 
+ * Copyright 2021 André Luis <André Luis@DESKTOP-HDL2CBS>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+#include <cstdlib>
+#include "./include/ExecTime.h"
+#include "./example/RCPMS.h"
+#include "./include/PT.h"
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	//varibles
+	float tempIni = 0.01;
+	float tempfim = 2.0;
+	int tempN = 10;
+	int MCL = 0;
+	int PTL = 2;	
+	int tempUp = 50;
+	int tempD = 1;
+	int uType = 0;
+	int thN = thread::hardware_concurrency();	
+	vector<std::string> arguments(argv + 1, argv + argc);	
+	
+	// Instance file name
+	std::string fn = arguments[0];
+	
+	// Read arguments
+	for(unsigned int i=1; i<arguments.size(); i+=2)
+	{
+		            
+        if(arguments[i]== "--TEMP_FIM")
+            tempfim = std::stof(arguments[i+1]);
+        else if(arguments[i]== "--TEMP_INIT")
+            tempIni = std::stof(arguments[i+1]);
+        else if(arguments[i]== "--N_REPLICAS")
+            tempN = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--MCL")
+            MCL  = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--PTL")
+            PTL = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--TEMP_DIST")
+            tempD = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--TYPE_UPDATE")
+            uType = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--TEMP_UPDATE")
+            tempUp = std::stoi(arguments[i+1]);
+        else if(arguments[i]== "--THREAD_USED")
+            thN = std::stoi(arguments[i+1]);
+    }
+	
+	// Create SSP object
+	RCPMS* prob = new RCPMS(fn);
+	
+	// Create and start PT 
+	PT<solRCPMS> algo(tempIni,tempfim,tempN,MCL,PTL,tempD,uType,tempUp);
+	ExecTime et;
+	solRCPMS sol = algo.start(thN, prob);
+	std::cout<<sol.evalSol<<"\n";
+	et.printTime();
+
+	return 0;
+}
+
